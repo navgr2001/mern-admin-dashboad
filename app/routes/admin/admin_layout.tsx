14b5505942c9e095db476cffea3e708a9d6327c4
navgr2001 <naveenshalom2001@gmail.com>
2025-09-01 00:08:56 -0700

    setup

---
+++ app/routes/admin/admin_layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const admin_layout = () => {
+  return (
+    <div className="admin-layout">
+      Mobile Sidebar
+      <aside className="w-full max-w-[270px] hidden lg:block"></aside>
+    </div>
+  );
+};
+
+export default admin_layout;
---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const dashboard = () => {
+  return <div>Dashboard Page</div>;
+};
+
+export default dashboard;
