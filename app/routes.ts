c47a1d10f68e6f4c135a86aa6524157bea2a8a02
navgr2001 <naveenshalom2001@gmail.com>
2025-09-01 06:23:03 -0700

    setup

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,6 @@ import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 export default [
   layout("routes/admin/admin_layout.tsx", [
     route("dashboard", "routes/admin/dashboard.tsx"),
+    route("all_users", "routes/admin/all_users.tsx"),
   ]),
 ] satisfies RouteConfig;
---
+++ app/routes/admin/all_users.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const all_users = () => {
+  return <div>Users Table</div>;
+};
+
+export default all_users;
